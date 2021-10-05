cbf3dbc42aa9398107969d3f7e0b5874a9da29b3
Chris De montigny <chrisdemontigny@yahoo.com>
2021-10-05 12:09:51 -0600

    adding responsive properties to user info dash

--- src/Components/UserPage/UserPage.js
+++ src/Components/UserPage/UserPage.js
@@ -96,8 +96,8 @@ class UserPage extends Component {
             {/* {!isLoggedIn ? (<div className="blue-screen-of-death">{alert("please log i")}</div>) : ( */}
             {!isLoggedIn ? (<Route path="/" component={Home}/>) : (
         <div className="user-page">
-            <ul className="column1">
-                <li><div className="portrait">
+            <div className="column1">
+                <div className="portrait">
                 <img className="model-img profile-photo" 
                 src={photo}
                 alt="photo"/>
@@ -113,45 +113,45 @@ class UserPage extends Component {
                 {/* > */}
                     {/* add photo */}
                 {/* </input> */}
-
-                </div></li>
-                <li><h2 className="name-container" >{this.props.user.user.name}</h2></li>
-                <li><div onClick={() => this.hideView('showUserInfo')} className="profile-buttons">user info</div></li>
-                <li><div onClick={() => this.hideView('showCollections')} className="profile-buttons">collections</div></li>
-                <li><div className="profile-buttons">groups</div></li>
-                <li><div className="profile-buttons">friends</div></li>
-                {/* <li><div className="profile-buttons">stuff</div></li> */}
-            </ul>
-            <section className="column2">
-
-                <div className="show-user-small">
-                    <ul>
-                    <li><div className="portrait">
-                    <img className="model-img profile-photo" 
-                    src={photo}
-                    alt="photo"/>
-                    {/* <svg className="icon-big" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg> */}
-
-                    {/* <input */}
-                    {/* // type="file" */}
-                    {/* // accept="image/pne,image/jpeg" */}
-                    {/* // onChange={e => this.handlePhoto(e)} */}
-                    {/* > */}
-                        {/* add photo */}
-                    {/* </input> */}
-
-                    </div></li>
-                    <li><h2 className="name-container" >{this.props.user.user.name}</h2></li>
+                <h2 className="name-container" >{this.props.user.user.name}</h2>
+                </div>
+                <ul>
                     <li><div onClick={() => this.hideView('showUserInfo')} className="profile-buttons">user info</div></li>
                     <li><div onClick={() => this.hideView('showCollections')} className="profile-buttons">collections</div></li>
                     <li><div className="profile-buttons">groups</div></li>
                     <li><div className="profile-buttons">friends</div></li>
                     {/* <li><div className="profile-buttons">stuff</div></li> */}
-                    </ul>
+                </ul>
+            </div>
+            <section className="column2">
+
+            <div className="show-user-small">
+                <div className="portrait">
+                <img className="model-img profile-photo" 
+                src={photo}
+                alt="photo"/>
+                {/* <svg className="icon-big" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg> */}
+
+                {/* <input */}
+                {/* // type="file" */}
+                {/* // accept="image/pne,image/jpeg" */}
+                {/* // onChange={e => this.handlePhoto(e)} */}
+                {/* > */}
+                    {/* add photo */}
+                {/* </input> */}
+                <h2 className="name-container" >{this.props.user.user.name}</h2>
                 </div>
+                <ul>
+                    <li><div onClick={() => this.hideView('showUserInfo')} className="profile-buttons">user info</div></li>
+                    <li><div onClick={() => this.hideView('showCollections')} className="profile-buttons">collections</div></li>
+                    <li><div className="profile-buttons">groups</div></li>
+                    <li><div className="profile-buttons">friends</div></li>
+                    {/* <li><div className="profile-buttons">stuff</div></li> */}
+                </ul>
+            </div>
 
                 {showCollections && <Collections username={this.props.user}/>}
                 
