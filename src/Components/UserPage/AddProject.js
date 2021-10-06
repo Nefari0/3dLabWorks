0ee2acefc9de2a15c03031e208e7e0405f6f2b34
Chris De montigny <chrisdemontigny@yahoo.com>
2021-10-06 09:13:11 -0600

    styling to add project form for UX

--- src/Components/UserPage/AddProject.js
+++ src/Components/UserPage/AddProject.js
@@ -5,24 +5,25 @@ const AddProject = (props) => {
     return(
         <div className="add-project">
             <p>add project</p>
-            <div className="add-project-button">
-                <form>
-                <input 
-                type="file"
-                accept="image/png,image/jpeg"
-                onChange={e => props.handlePhoto(e)} 
-                />
-                
-                </form>
-            </div>
-            {/* <p>add file</p> */}
+            <a className="small-text">image</a>
             <div className="add-project-button">
                 <input
-                type="file"
-                accept=".stl,.blend"
-                onChange={e => props.handleFile(e)} 
+                    type="file"
+                    accept="image/png,image/jpeg"
+                    onChange={e => props.handlePhoto(e)} 
+                />
+                
+            </div>
+            <a className="small-text">.blend/.stl</a>
+            <div className="add-project-button">
+                <input
+                    type="file"
+                    accept=".stl,.blend"
+                    onChange={e => props.handleFile(e)} 
                 />
             </div>
+            <input placeholder="name"/>
+            <input placeholder="description"/>
             
             <div className="add-project-button">
                 <button onClick={props.fileHandler}>submit</button>
