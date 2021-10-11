92004e9b105ea822df49ce8d79308331c6aadfda
Chris De montigny <chrisdemontigny@yahoo.com>
2021-10-11 17:28:18 -0600

    testing icon

--- src/Components/FeaturedProjects/Project.js
+++ src/Components/FeaturedProjects/Project.js
@@ -57,9 +57,9 @@ const Project = (props) => {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
                 </svg></a> */}
 
-                <a><svg xmlns="http://www.w3.org/2000/svg" className="big" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg className="small-icon" style={{margin:'auto'}} xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-                </svg></a>
+                </svg>
 
                 <a className="project-text" href={`${firebase_url}`}>download</a>
                 {/* <div className="horizontal-devide"></div> */}
