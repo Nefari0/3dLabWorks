5301fc3d8e82bbafcd2673903dc234797c89a15a
Chris De montigny <chrisdemontigny@yahoo.com>
2021-10-19 10:33:47 -0600

    styling. preparing to add conditional display feature for admin users

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,15 +4,20 @@ import './About.css'
 import Document from './../Document'
 import axios from 'axios'
 import data from '../../data';
+import { connect } from 'react-redux'
+import { updateUser } from '../../ducks/userReducer';
+import UserPage from '../UserPage/UserPage';
 
-export default class About extends Component {
+class About extends Component {
 
     constructor(){
         super();
 
         this.state = {
             about:[],
+            showAbout:true,
             general:[],
+            showGeneral:true
         }
     }
 
@@ -40,25 +45,20 @@ export default class About extends Component {
 
             return(
                 <div className="about-container">
-                    {/* <div className="doc-containers">
-                        <h4 className="about-h4">about</h4>
-                        <div className="h4-border"></div>
-                        <p>
-                            MadModels is an establishment to allow makers, tinkerers and artist to updload and share 3d models. Ultimately, the purpose of this site is to act as a repository, where makers can share their work, and invite other makers to edit and collaborate on projects. The formats used in this site are .stl and .blend. 
-                        </p>
-                    </div> */}
+                    
                     {mappedAbout}
-                    {/* <div className="doc-containers">
-                        <h4 className="about-h4">General Information</h4>
-                        <div className="h4-border"></div>
-                        <text>
-                            This site is an ongoing project. As of 10/4/2021 it is still under construction. Developers are working towards utilizing in app 3d rendering tools to allow users to upload .stl and .mtl files that will automatically be displayed in the app. Since this is currently not supported, it is recommended that you upload a screenshot of your project along with the file so other users can preview your work before downloading the file
-                        </text>
-                    </div> */}
+
                     {mappedGeneral}
+
                 </div>
             )
         }
 }
+
+function mapStateToProps(reduxState){
+    return reduxState
+}
+
+export default connect(mapStateToProps,{updateUser})(About)
 
 // export default class About
