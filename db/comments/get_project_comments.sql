927df748e016f8bec1c229486da2161637e366d9
Chris De montigny <chrisdemontigny@yahoo.com>
2021-10-25 11:18:55 -0600

    read and write comments

--- db/comments/get_project_comments.sql
+++ db/comments/get_project_comments.sql
@@ -1,2 +1,3 @@
 SELECT * FROM comments WHERE model_id = $1
+-- JOIN d_user d ON c.user_id = d.user_id
 order by date_created desc;
--- db/projects/create_comment.sql
+++ db/projects/create_comment.sql
@@ -1,3 +1,3 @@
-INSERT INTO comments (user_id,model_id,text)
-VALUES ($1,$2,$3)
+INSERT INTO comments (user_id,model_id,user_name,text)
+VALUES ($1,$2,$3,$4)
 RETURNING *;
