e012da7c1719629ddb6127946e3a6cecbb6d0a45
Nefari0 <chrisdemontigny@yahoo.com>
2021-12-08 14:07:09 -0700

    edited svg code

--- src/Components/MobileLogin/MobileLogin.js
+++ src/Components/MobileLogin/MobileLogin.js
@@ -28,12 +28,12 @@ const MobileLogin = (props) => {
             <p style={{fontSize:'10px',marginBottom:'5px',marginLeft:'10px'}} >stay logged in?</p>
 
             {!saveSession ?
-            <svg onClick={() => props.setSaveSession()} style={{marginLeft:'10px',marginRight:'20px', height:'20px',width:'20px',opacity:'60%',marginTop:'10px',marginBottom:'2px'}} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+            <svg onClick={() => props.setSaveSession()} style={{marginLeft:'10px',marginRight:'20px', height:'20px',width:'20px',opacity:'60%',marginTop:'10px',marginBottom:'2px'}} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20" fillRule="currentColor">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
             </svg>
             :
-            <svg onClick={() => props.setSaveSession()} style={{marginLeft:'10px',marginRight:'20px', height:'20px',width:'20px',opacity:'60%',marginTop:'10px',marginBottom:'2px'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            <svg onClick={() => props.setSaveSession()} style={{marginLeft:'10px',marginRight:'20px', height:'20px',width:'20px',opacity:'60%',marginTop:'10px',marginBottom:'2px'}} xmlns="http://www.w3.org/2000/svg" fillRule="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>}
 
         </ul>
