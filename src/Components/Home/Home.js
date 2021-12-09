300dc497e3770f08f26dee8907a4b53c57ac09bf
Nefari0 <chrisdemontigny@yahoo.com>
2021-12-09 11:43:01 -0700

    styling/size adjustments

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -28,19 +28,15 @@ class Home extends Component {
         this.handlePassword = this.handlePassword.bind(this)
         this.handleClick = this.handleClick.bind(this)
         this.remindWhoUser = this.remindWhoUser.bind(this)
-        // this.likeFunc = this.likeFunc.bind(this)
     }
 
     componentDidMount() {
 
         this.props.getModels()
 
-        // axios.get('api/project/join').then(res => 
-        // this.setState({...this.state,projects:res.data}))
     }
 
     componentDidUpdate() {
-        // this.props.getModels()
         this.props.updateUser()
     }
 
@@ -63,37 +59,16 @@ class Home extends Component {
         console.log(this.props.user.user.data)
     }
 
-    // likeFunc = (id,m_id) => {
-    //     const user_id = id
-    //     const model_id = m_id
-    //     axios.post('/api/projects/like', { user_id,model_id })
-    // }
-
     render() {
         const { user_name, password, projects } = this.state
         const { loginUser } = this.props
         const { isLoggedIn } = this.props.user
         const { user_likes,model_likes,id } = this.props.user.user
         const { models } = this.props.models
-        // console.log(this.props) 
 
         const mappedModels = models.map(element => {
             return <Project data={element} key={element.model_id} isLoggedIn={isLoggedIn} model_likes={model_likes} likes={element.likes} id={id} user_likes={user_likes} />
         })
-
-        // {if (user_likes != undefined){mappedModels.forEach(element => {
-        //     for (let i = 0; i < user_likes.length; i++){
-        //         if(element.model_id === user_likes.model_id) {
-        //             mappedModels.isLikedByUser = true
-        //             // console.log('model like',user_likes[i])
-        //         }
-        //     }
-        // })}}
-
-        // const mappedModels = projects.map(element => {
-        //     // return <Project data={element} key={element.user_id}/> // original
-        //     return <Project data={element} key={element.model_id} isLoggedIn={isLoggedIn} model_likes={model_likes} likes={element.likes} id={id} />
-        // })
 
         return(
             <div>
@@ -113,41 +88,6 @@ class Home extends Component {
                         <source src={Vid} type="video/mp4"/>
                     </video> */}
                 </div>
-                <Footer/>
-                {/* <FeaturedProjects/> */}
-                {/* <div className="title-border"></div>
-                <section className="intro-text">
-                    <h3 className="icon-h3">Join millions  of 3d makers worldwide. Share your work in a community, and be a part of an innovative future</h3>
-                </section>
-
-                <section className="homw-container">
-                <div classname="icon-container">
-                <svg className="icon"xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
-                </svg>
-                <h4 className="icon-h4">SHARE YOUR WORK</h4>
-                <p className="icon-text" >Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-
-                <svg className="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
-                </svg>
-                <h4 className="icon-h4">COLLABORATE ON PROJECTS</h4>
-                <p className="icon-text" >Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-
-                <svg className="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
-                </svg>
-                <h4 className="icon-h4">BUILD YOUR ASSET LIBRARY</h4>
-                <p className="icon-text" >Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-
-                <svg className="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-                </svg>
-                <h4 className="icon-h4">ENHANCE YOUR KNOWLEDGE</h4>
-                <p className="icon-text" >Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-
-                </div>
-                </section> */}
             </div>
         )
     }
