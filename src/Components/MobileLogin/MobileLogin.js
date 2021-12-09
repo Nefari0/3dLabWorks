ebfd58c933adf018f82f819d5f3c8189683e15b2
Nefari0 <chrisdemontigny@yahoo.com>
2021-12-09 09:01:50 -0700

    cleaned code

--- src/Components/MobileLogin/MobileLogin.js
+++ src/Components/MobileLogin/MobileLogin.js
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react'
 import './MobileLogin.css'
-// import { connect } from 'react-redux'
-// import { loginUser, logoutUser } from '../../ducks/userReducer'
 import { Link } from 'react-router-dom'
 
 const Prototype = (props) => {
@@ -15,10 +13,6 @@ const Prototype = (props) => {
     return(
         <div className={`plogin-container`}>
         <section className="login-title">
-            
-            {/* <svg className="close-button" style={{color:'#fff', height:'35px',width:'35px',opacity:'60%',marginTop:'2px',marginBottom:'2px'}} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20" >
-                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
-            </svg> */}
 
             <svg className="close-button" style={{color:'#fff', height:'35px',width:'35px',opacity:'60%',marginTop:'2px',marginBottom:'2px'}} onClick={props.exit} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
