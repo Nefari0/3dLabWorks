52196cfade1939a49a2e31367e167cf6c0a763fb
Nefari0 <chrisdemontigny@yahoo.com>
2021-12-09 22:37:08 -0700

    cleaned code

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -173,9 +173,8 @@ class Header extends Component{
                 {!isLoggedIn ? (<div></div>) : (<Link to="/user" style={{ textDecoration: 'none' }}><li className='link-item'><a>my page</a></li></Link>)}
             </ul>
 
-            <div className="login-link" onClick={this.toggleLogin}>
-                {!isLoggedIn ? (<h4 className="login-logout">login</h4>):(<h4 className="login-logout">logout</h4>)}
-            </div>
+            {!isLoggedIn ? (<h4 className="login-link" onClick={this.toggleLogin} >Login</h4>):(<h4 className="login-link" onClick={this.toggleLogin} >Log Out</h4>)}
+
 
             <svg className="hamburger" onClick={this.toggleMenu} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
