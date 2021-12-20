1aef4b2ca5cee4a9dad84e451cbf3eef462a27d1
Nefari0 <chrisdemontigny@yahoo.com>
2021-12-20 08:11:24 -0700

    converted ul tags to div tags

--- src/Components/FeaturedProjects/Project.js
+++ src/Components/FeaturedProjects/Project.js
@@ -86,14 +86,12 @@ const Project = (props) => {
 
                     <div className="horizontal-devide"></div>
                     <div className="like-share">
-                        <ul>
+                        {/* <ul>
                         <li className="like-share-box"><p className="like-share-text">
 
                         <svg className="small-icon" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"  />
                         </svg>
-                        
-                        {/* {likes}</p></li> */}
                         
                         {numOfLikes}</p>
                         </li>
@@ -105,7 +103,27 @@ const Project = (props) => {
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
                         </svg>
                         share</p></li>
-                        </ul>
+                        </ul> */}
+
+                    <div>
+                        <div className="like-share-box"><p className="like-share-text">
+
+                        <svg className="small-icon" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"  />
+                        </svg>
+                        
+                        {numOfLikes}</p>
+                        </div>
+
+                        <div className="devide"></div>
+
+                        <div className="like-share-box"><p className="like-share-text">
+                        <svg className="small-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
+                        </svg>
+                        share</p></div>
+                        </div>
+
                     </div>
                 </div>
             </div>
