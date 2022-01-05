e7e7d61a9d4846a3276d67963e679833f91feeb0
Nefari0 <chrisdemontigny@yahoo.com>
2022-01-04 22:42:41 -0700

    Notice displays well

--- src/Components/Notice/Notice.js
+++ src/Components/Notice/Notice.js
@@ -5,8 +5,7 @@ import { useState,useEffect } from 'react'
 const Notice = (props) => {
 
     useEffect(() => {
-        // setTimeout(setShowNotice(false), 10000)
-        // setShowNotice(false)
+        setShowNotice(false)
     },[])
 
     const [ showNotice, setShowNotice ] = useState(true)
@@ -19,9 +18,7 @@ const Notice = (props) => {
 
     return (
         <div className={`notice-container ${item} ${showNotice ? true : 'hidden'}`}>
-            <div className='notice-container-outer '></div>
-            {/* <h2 style={{color:'#555'}}>text</h2> */}
-            <p style={{color:'#555',textTransform:'none'}}>{content}</p>
+            <p style={{color:'#fff',textTransform:'none'}}>{content}</p>
         </div>
     )
 }
