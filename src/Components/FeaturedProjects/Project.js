0ffcfddd1543edb5f011f675dc171fd25148a27a
Nefari0 <chrisdemontigny@yahoo.com>
2022-01-05 00:20:18 -0700

    styling adjustments

--- src/Components/FeaturedProjects/Project.js
+++ src/Components/FeaturedProjects/Project.js
@@ -84,25 +84,32 @@ const Project = (props) => {
                 <div className="image-div" ><Link to={`projectdetails/${model_id}`}><img className="img" src={firebase_url01}/></Link></div>
             </div>
             <div>
-                <div className="download-container-top"></div>
                 <div className="download-container">
-                    {/* <svg className="big" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                    </svg> */}
-                    {/* <a style={{margin:"auto"}} href={`${firebase_url}`} ><svg className="big" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
-                    </svg></a> */}
+                {/* <div className="download-container-top"> */}
 
-                    { isLoggedIn ? <a href={`${firebase_url}`} ><svg className="small-icon" style={{marginLeft:'10px',marginRight:'20px', height:'50px',width:'50px',opacity:'60%'}} xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    {/* { isLoggedIn ? <a href={`${firebase_url}`} ><svg className="small-icon" style={{marginLeft:'10px',marginRight:'20px', height:'50px',width:'50px',opacity:'60%'}} xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                     </svg></a> : <a onClick={plsSignIn} ><svg className="small-icon" style={{marginLeft:'10px',marginRight:'20px', height:'50px',width:'50px',opacity:'60%'}} xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                     </svg></a>}
 
                     
+                    { isLoggedIn ? <a className="project-text" href={`${firebase_url}`}>download</a> : <a className="project-text" onClick={plsSignIn}>download</a>} */}
+
+                    {/* <div className="horizontal-devide"></div> */}
+                {/* </div> */}
+
+                { isLoggedIn ? <a href={`${firebase_url}`} ><svg className="small-icon" style={{marginLeft:'10px',marginRight:'5px', height:'50px',width:'50px',opacity:'60%'}} xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                    </svg></a> : <a onClick={plsSignIn} ><svg className="small-icon" style={{marginLeft:'10px',marginRight:'5px', height:'50px',width:'50px',opacity:'60%'}} xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                    </svg></a>}
+
+                    
                     { isLoggedIn ? <a className="project-text" href={`${firebase_url}`}>download</a> : <a className="project-text" onClick={plsSignIn}>download</a>}
 
-                    <div className="horizontal-devide"></div>
+                    {/* <div className="horizontal-devide"></div> */}
+
                     <div className="like-share">
                         {/* <ul>
                         <li className="like-share-box"><p className="like-share-text">
@@ -125,25 +132,30 @@ const Project = (props) => {
 
                     <div>
                         {/* {checkIfLiked()} */}
-                        <div className="like-share-box"><p className="like-share-text">
-                        {checkIfLiked() === false ? <svg className="small-icon" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+                        <div className="like-share-box" style={{borderBottom:'1px solid #555'}}>
+                            {/* <p className="like-share-text"> */}
+                        {checkIfLiked() === false ? <svg className="small-icon small-icon-tweaks" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"  />
                         </svg>
                         :
-                        <svg className="small-icon" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <svg className="small-icon small-icon-tweaks" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
                         </svg>}
-                        
+                        <p className="like-share-text" style={{marginLeft:'10px'}}>
                         {numOfLikes}</p>
                         </div>
 
-                        <div className="devide"></div>
+                        {/* <div className="devide"></div> */}
 
-                        <div className="like-share-box"><p className="like-share-text">
-                        <svg className="small-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <div className="like-share-box">
+                            {/* <p className="like-share-text"> */}
+                        <svg className="small-icon small-icon-tweaks" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
                         </svg>
-                        share</p></div>
+                        <p className="like-share-text" style={{marginLeft:'10px'}}>
+                        share</p>
+                        {/* <p style={{color:'#555'}}>text</p> */}
+                        </div>
                         </div>
 
                     </div>
