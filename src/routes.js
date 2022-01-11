7516ac7ae81fbc4c13362852959dedd1a4b46c12
Nefari0 <chrisdemontigny@yahoo.com>
2022-01-11 14:15:13 -0700

    new route to "messages" view

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,7 @@ import ProjectDetail from './Components/FeaturedProjects/ProjectDetail'
 import SecurityTest from './Components/UserPage/SecurityTest'
 import Agreement from './Components/Register/Agreement'
 import AdminPage from './Components/AdminPage/AdminPage'
+import Messages from './Components/Messages/Messages'
 
 
 export default (
@@ -30,5 +31,6 @@ export default (
         <Route path="/header" component={Header} />
         <Route path="/agreement" component={Agreement} />
         <Route path="/admin" component={AdminPage} />
+        <Route path="/messages/:user_id" component={Messages} />
     </Switch>
 )
