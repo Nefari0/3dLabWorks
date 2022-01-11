69649549e4545e1d57b5f3da3a39ee31f4ebe255
Nefari0 <chrisdemontigny@yahoo.com>
2022-01-11 14:17:52 -0700

    add and delete messages

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,10 +1,23 @@
 
 module.exports = {
     getUserMessages: async (req,res) => {
-        console.log('hit controller')
         const { user_id } = req.params
         const db = req.app.get('db')
         const messages = await db.messaging.get_messages_from_user([user_id])
         return res.status(200).send(messages)
+    },
+
+    createMessage: async (req,res) => {
+        const { user_id, text, message_discription, send_from } = req.body
+        const db = req.app.get('db')
+        const message = await db.messaging.add_message([user_id,text,message_discription,send_from])
+        return res.status(200).send(message)
+    },
+
+    deleteMessage: async (req,res) => {
+        const { message_id,user_id } = req.body
+        const db = req.app.get('db')
+        const query = await db.messaging.delete_message([message_id])
+        return res.status(200).send(query)
     }
 }
