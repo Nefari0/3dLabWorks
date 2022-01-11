1783535d3153491a1125492c7c14874d6bf8d8c3
Nefari0 <chrisdemontigny@yahoo.com>
2022-01-11 14:20:19 -0700

    now routes to message view with the selected user

--- src/Components/AdminPage/ShowUser.js
+++ src/Components/AdminPage/ShowUser.js
@@ -1,14 +1,15 @@
 import './AdminPage.css'
+import { Link } from 'react-router-dom'
 
 const ShowUser = (props) => {
 
-    const { user_name,first_name,last_name } = props
+    const { user_name,first_name,last_name,user_id } = props
 
     return(
-        <div className="show-user">
+        <Link to={`/messages/${user_id}`}><div className="show-user">
             <p className="admin-a">{first_name + ' ' + last_name}</p>
             <p className="admin-a">{user_name}</p>
-        </div>
+        </div></Link>
     )
 }
 
