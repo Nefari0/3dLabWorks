4b7d8083e5b0911621806d0ae78efbb404e55299
Nefari0 <chrisdemontigny@yahoo.com>
2022-01-21 05:15:32 -0700

    select photo thumbnails to highlight

--- src/Components/FeaturedProjects/ProjectPhotos.js
+++ src/Components/FeaturedProjects/ProjectPhotos.js
@@ -6,7 +6,7 @@ const ProjectPhotos = (props) => {
 
     const { firebase_url01, firebase_url, name } = props.data
     const { first_name, photo_url, user_name } = props.userInfo[0]
-    const { isLoggedIn } = props
+    const { isLoggedIn,url } = props
     
 
     return(
@@ -16,7 +16,8 @@ const ProjectPhotos = (props) => {
                 <img src={photo_url} className="logo-box"/>
                 <p className="dark-text">{name}<br/>by {user_name}</p>
             </div> */}
-            <img src={firebase_url01} className="detail-photo"/>
+            {/* <img src={firebase_url01} className="detail-photo"/> */}
+            <img src={url} className="detail-photo"/>
             {/* {isLoggedIn ? <a className="dark-text show-dl-url" href={`${firebase_url}`}>Download</a> : <a className="dark-text show-dl-url" onClick={props.plsSignIn} >Download</a>} */}
         </div>
         // </div>
