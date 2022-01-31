ad769a86598f4f1e4b7d690d2fd8ad9aa032ab29
Nefari0 <chrisdemontigny@yahoo.com>
2022-01-31 10:59:12 -0700

    like icon highlights in real time

--- src/Components/FeaturedProjects/Project.js
+++ src/Components/FeaturedProjects/Project.js
@@ -8,10 +8,9 @@ import { updateUser } from '../../ducks/userReducer'
 // import { addLike } from '../../../server/controllers/projectsController'
 
 const Project = (props) => {
-    // const { category, img, firebase_url,firebase_url01, name, description, user_name, model_likes, model_id, photo_url, user_id, likes } = props.data
+
     const { category, img, firebase_url,firebase_url01, name, description, user_name, model_likes, model_id,photo_url, user_id, likes } = props.data
     const { handleClick,isLoggedIn,id,user_likes } = props
-    // const { user_likes } = props.user.user
 
     const [numOfLikes, setNumOfLikes] = useState(0)
     const [heart,setHeart] = useState(false) // for highlighting hearts if current user has already like this project
@@ -19,6 +18,11 @@ const Project = (props) => {
     useEffect(() => {
         
         setNumOfLikes(likes)
+
+        if(checkIfLiked() === true && heart === false) {
+            setHeart(true)
+        }
+
     },[])
 
     const plsSignIn = () => {
@@ -48,9 +52,6 @@ const Project = (props) => {
     }
 
     const checkIfLiked = (params) => {
-        // console.log('clicked "check if liked"')
-        console.log('clicked check if likeds',params)
-        // if(params != undefined) {return(params)}
         if (props.projectIsLiked(model_id,user_likes) === true){ return(true) } else { return(false) } 
     }
 
@@ -72,20 +73,8 @@ const Project = (props) => {
         <div className='project-container'>
             {checkIfLiked()}
             <div>
-            {/* <p className="dark-text-larger">{name}</p> */}
-                {/* <div className="photo-title-border"><img src={photo_url} className="project-user-photo"/><p className="dark-text-larger">{name}<br/><p className='usr-nm-txt'>by {user_name}</p></p></div> */}
-
-
-                {/* <div className="photo-title-border"><img src={photo_url} className="project-user-photo"/> */}
-                {/* <div className="photo-title-border"><img src={photo_url} className="project-user-photo"/> */}
-
-                {/* --------------- */}
                 <div className="photo-title-border"><Link to={`viewuser/${user_id}`} ><img src={getPhotoUrl()} className="project-user-photo"/></Link>
-                {/* <div className="image-div" ><Link to={`viewuser/${user_id}`}><img className="model-photo" src={photo_url}/></Link></div> */}
-                {/* ------------- */}
-                
-                {/* <p className="dark-text-larger">{name}</p> */}
-                {/* <p className="dark-text">{name}<br/><p className='usr-nm-txt'>by {user_name}</p></p></div> */}
+
                 <p className="dark-text">{name}<br/><p className='usr-nm-txt'>by {getUserName()}</p></p></div>
 
                 <div className="image-div" ><Link to={`projectdetails/${model_id}`}><img className="model-photo" src={firebase_url01}/></Link></div>
@@ -110,14 +99,23 @@ const Project = (props) => {
 
                     <div className="like-share">
                         <div>
+
                             <div className="like-share-box" style={{borderBottom:'1px solid #555'}}>
-                                {checkIfLiked() === false ? <svg className="small-icon small-icon-tweaks" onClick={() => clickedLike()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+                                {
+
+                                heart === false ?
+                            
+                                
+                                <svg className="small-icon small-icon-tweaks" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" fill='none' viewBox="0 0 24 24" stroke="currentColor" >
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"  />
                                 </svg>
                                 :
                                 <svg className="small-icon small-icon-tweaks" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-                                </svg>}
+                                </svg>
+
+
+                                }
                                 <p className="like-share-text" style={{marginLeft:'10px'}}>{numOfLikes}</p>
                             </div>
 
@@ -141,5 +139,3 @@ function mapStateToProps(reduxState) {
 }
 
 export default connect(mapStateToProps, { getModels,updateUser })(Project)
-
-// export default Project
