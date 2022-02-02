ac702bf071de49d6287f53b58f7a1b90d6658481
Nefari0 <chrisdemontigny@yahoo.com>
2022-02-01 23:48:49 -0700

    creates new conversation when visiting public user page

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -37,5 +37,33 @@ module.exports = {
         const db = req.app.get('db')
         const messages = await db.messaging.get_conversation_messages_by_id([conversation_id])
         return res.status(200).send(messages)
-    }
+    },
+
+    createNewConversation: async (req,res) => {
+        const { text,id,user_id } = req.body
+        const content = text
+        const conversation_name = 'new conversation'
+        const db = req.app.get('db')
+        console.log('here is id',user_id)
+        const newConversation = await db.messaging.create_new_conversation([conversation_name])
+        const conversation = newConversation[0]
+        const { conversation_id } = conversation
+        
+        const userConversationFrom = await db.messaging.create_user_conversation([id,conversation['conversation_id'],user_id])
+        const fromUser = userConversationFrom[0]
+        const userConversationTo = await db.messaging.create_user_conversation([user_id,conversation['conversation_id'],id])
+        const toUser = userConversationTo[0]
+        
+        const message = await db.messaging.create_conversation_message([id,conversation['conversation_id'],text])
+        return res.status(200).send(message)
+
+    },
+
+    sendMessage: async (req,res) => {
+        const db = req.app.get('db')
+        const { user_id,conversation_id,text } = req.body
+        const conversation = await db.messaging.create_conversation_message([user_id,conversation_id,text])
+        const info = conversation[0]
+        return res.status(200).send(info)
+    },
 }
