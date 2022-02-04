bd2c2ff3413f7fc354217743a361673d14be9af4
Nefari0 <chrisdemontigny@yahoo.com>
2022-02-03 17:21:04 -0700

    in the process of adding highlighting effects to selected messages

--- src/Components/UserMessages/SelectedMessage.js
+++ src/Components/UserMessages/SelectedMessage.js
@@ -3,15 +3,27 @@ import { useState,useEffect } from 'react'
 
 const SelectedMessage = (props) => {
 
-    const { photo_url,conversation_id,SelectedMessage } = props
+    const { photo_url,conversation_id,selectedMessage } = props
     const [isSelected,setIsSelected] = useState(false)
     
+    useEffect(() => {
+        if(selectedMessage===conversation_id && isSelected===false) {setIsSelected(true)} else if (selectedMessage === conversation_id && isSelected===true) {setIsSelected(false)}
+        // setIsSelected(false)
+    },[])
 
-    if(SelectedMessage===conversation_id) {setIsSelected(true)}
+    const gotClicked = () => {
+        // setIsSelected(true)
+        props.openMessage(conversation_id)
+    }
 
-    return(<div className='selected-message-container' onClick={() => props.openMessage(conversation_id)} >
-        <img className='senders-photo' src={photo_url} />
-    </div>)
+    return(
+        // isSelected ?
+        <div className={`selected-message-container`} onClick={() => gotClicked()} >
+            <img className='senders-photo' src={photo_url} />
+        </div>
+        //    : 
+        //   <div className='selected-message-selected' onClick={() => gotClicked()} ></div>
+          )
 }
 
 export default SelectedMessage
