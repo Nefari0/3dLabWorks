a2bf230685cbbc17157f1c397f7c3a218928a45c
Nefari0 <chrisdemontigny@yahoo.com>
2022-02-08 23:53:55 -0700

    phasing this component out

--- src/Components/UserMessages/EndOfMessages.js
+++ src/Components/UserMessages/EndOfMessages.js
@@ -1,9 +1,9 @@
-import useRef from 'react'
-import React from 'react'
-const EndOfMessages = () => {
-    // const myRef = useRef();
-    // const myRef = () => React.useRef();
-    const fieldRef= () => useRef;
-    return(<div ref={fieldRef()} id='theEnd' ></div>)
-}
-export default EndOfMessages
+// import useRef from 'react'
+// import React from 'react'
+// const EndOfMessages = () => {
+//     // const myRef = useRef();
+//     // const myRef = () => React.useRef();
+//     const fieldRef= () => useRef;
+//     return(<div ref={fieldRef()} id='theEnd' ></div>)
+// }
+// export default EndOfMessages
