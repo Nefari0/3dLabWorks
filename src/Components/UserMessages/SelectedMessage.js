92f2d88dde350c9ae1c0bf6fff9c6d4eee07861d
Nefari0 <chrisdemontigny@yahoo.com>
2022-02-08 23:57:58 -0700

    styling adjustments

--- src/Components/UserMessages/SelectedMessage.js
+++ src/Components/UserMessages/SelectedMessage.js
@@ -3,23 +3,26 @@ import { useState,useEffect } from 'react'
 
 const SelectedMessage = (props) => {
 
-    const { photo_url,conversation_id,selectedMessage } = props
+    const { photo_url,conversation_id,selectedMessage,user_name } = props
     const [isSelected,setIsSelected] = useState(false)
     
     useEffect(() => {
-        if(selectedMessage===conversation_id && isSelected===false) {setIsSelected(true)} else if (selectedMessage === conversation_id && isSelected===true) {setIsSelected(false)}
+        // if(selectedMessage===conversation_id && isSelected===false) {setIsSelected(true)} else if (selectedMessage === conversation_id && isSelected===true) {setIsSelected(false)}
         // setIsSelected(false)
     },[])
 
-    const gotClicked = () => {
+    const gotClicked = (id) => {
         // setIsSelected(true)
-        props.openMessage(conversation_id)
+        props.openMessage(id)
     }
 
     return(
         // isSelected ?
-        <div className={`selected-message-container`} onClick={() => gotClicked()} >
+        // <div className={`selected-message-container ${!isSelected ? true : 'selected-message-selected'}`} onClick={() => gotClicked(conversation_id)} >
+        <div className={`selected-message-container ${selectedMessage!=conversation_id ? true : 'selected-message-selected'}`} onClick={() => gotClicked(conversation_id)} >
             <img className='senders-photo' src={photo_url} />
+            <p className='display-name-message'>{user_name}</p>
+            {/* <p className='display-name-message'>{user_name}</p> */}
         </div>
         //    : 
         //   <div className='selected-message-selected' onClick={() => gotClicked()} ></div>
