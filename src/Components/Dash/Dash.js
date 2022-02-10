d6aef028fdc50e1be52f7e3d8425c3cd6089f712
Nefari0 <chrisdemontigny@yahoo.com>
2022-02-09 22:30:01 -0700

    control board to prevent icons from blocking view

---
+++ src/Components/Dash/Dash.js
@@ -0,0 +1,12 @@
+import { useState,useEffect } from "react";
+import './Dash.css'
+import MessageBoard from "../UserMessages/MessageBoard";
+
+const Dash = (props) => {
+    
+    return(<div className="my-heard-bar " >
+        <MessageBoard/>
+    </div>)
+}
+
+export default Dash
