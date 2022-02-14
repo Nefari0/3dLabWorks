2bf81a8ad39702189d2e9b99f55d2b1e9b687028
Nefari0 <chrisdemontigny@yahoo.com>
2022-02-14 15:04:20 -0700

    routes to public user page

--- src/Components/AdminPage/ShowUser.js
+++ src/Components/AdminPage/ShowUser.js
@@ -6,7 +6,8 @@ const ShowUser = (props) => {
     const { user_name,first_name,last_name,user_id } = props
 
     return(
-        <Link to={`/messages/${user_id}`}><div className="show-user">
+        // <Link to={`/messages/${user_id}`}><div className="show-user">
+        <Link to={`/viewuser/${user_id}`}><div className="show-user">
             <p className="admin-a">{first_name + ' ' + last_name}</p>
             <p className="admin-a">{user_name}</p>
         </div></Link>
