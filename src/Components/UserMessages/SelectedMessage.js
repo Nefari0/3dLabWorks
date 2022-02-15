21c43bf92db4c16e34ffce758dbf556a775e5ba8
Nefari0 <chrisdemontigny@yahoo.com>
2022-02-15 15:53:40 -0700

    cleaned code

--- src/Components/UserMessages/SelectedMessage.js
+++ src/Components/UserMessages/SelectedMessage.js
@@ -7,25 +7,18 @@ const SelectedMessage = (props) => {
     const [isSelected,setIsSelected] = useState(false)
     
     useEffect(() => {
-        // if(selectedMessage===conversation_id && isSelected===false) {setIsSelected(true)} else if (selectedMessage === conversation_id && isSelected===true) {setIsSelected(false)}
-        // setIsSelected(false)
+
     },[])
 
     const gotClicked = (id) => {
-        // setIsSelected(true)
         props.openMessage(id)
     }
 
     return(
-        // isSelected ?
-        // <div className={`selected-message-container ${!isSelected ? true : 'selected-message-selected'}`} onClick={() => gotClicked(conversation_id)} >
         <div className={`selected-message-container ${selectedMessage!=conversation_id ? true : 'selected-message-selected'}`} onClick={() => gotClicked(conversation_id)} >
             <img className='senders-photo' src={photo_url} />
             <p className='display-name-message'>{user_name}</p>
-            {/* <p className='display-name-message'>{user_name}</p> */}
         </div>
-        //    : 
-        //   <div className='selected-message-selected' onClick={() => gotClicked()} ></div>
           )
 }
 
