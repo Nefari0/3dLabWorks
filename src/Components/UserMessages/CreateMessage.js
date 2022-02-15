1a6066acf459d8be05395de5eb84d6a3cfe675a0
Nefari0 <chrisdemontigny@yahoo.com>
2022-02-15 15:12:34 -0700

    push mew message to websockets for real-time communication

--- src/Components/UserMessages/CreateMessage.js
+++ src/Components/UserMessages/CreateMessage.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { Component } from "react";
 // import '../../Components/FeaturedProjects/Comments/Comments'
 import './CreateMessage.css'
+// import { w3cwebsocket as W3CWebSocket } from "websocket";
+// const client = new W3CWebSocket('ws://127.0.0.1:8000');
 
 
 export default class CreateMessage extends Component {
@@ -13,17 +15,15 @@ export default class CreateMessage extends Component {
             text:"",
         }
         this.handleText = this.handleText.bind(this)
-        // this.sendPost = this.sendPost.bind(this)
         this.executeSendMessage = this.executeSendMessage.bind(this)
     }
 
     executeSendMessage = async () => {
         const { text } = this.state
         const { user_id,conversation_id } = this.props
-        // console.log('hit execute',text,conversation_id,user_id)
 
         await axios.post('/api/conversation/user/new',{conversation_id,user_id,text})
-        await this.props.openMessage(conversation_id)
+        await this.props.sendToSockets(text,conversation_id)
         this.setState({text:''})
     }
 
@@ -46,8 +46,9 @@ export default class CreateMessage extends Component {
 
                 {/* {!isLoggedIn ? <div className='add-button file-button' onClick={this.props.plsSignIn} style={{marginLeft:'200px'}} >send</div> : <div className='add-button submit-message-button'  style={{marginLeft:'200px'}} onClick={() => this.executeSendMessage()} >send</div>} */}
                 <div className='send-message-button' onClick={() => this.executeSendMessage()} style={{position:'absolute',height:'30px',width:'30px'}} >
+                {/* <div className='send-message-button' onClick={() => this.props.sendToSockets()} style={{position:'absolute',height:'30px',width:'30px'}} > */}
                     {/* send */}
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
                     </svg>
                 </div> 
