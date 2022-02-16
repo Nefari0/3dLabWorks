a27ddd92067c4c5256802f3011bc71a255528cd8
Nefari0 <chrisdemontigny@yahoo.com>
2022-02-15 21:48:24 -0700

    <Loading /> displays while loggin in

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom'
 import MobileLogin from '../MobileLogin/MobileLogin'
 import UserPage from '../UserPage/UserPage'
 import { withRouter } from 'react-router'
+import Loading from '../Loading/Loading'
 
 class Header extends Component{
     constructor(props){
@@ -122,15 +123,15 @@ class Header extends Component{
 
     render() {
         const { saveSession, username, isMenuOpen, user_name, password, openLogin, isLoggedInState } = this.state
-        const { isLoggedIn } = this.props.user
+        const { isLoggedIn,isLoading } = this.props.user
 
     return(
         <div className='header-container'>
-
+            {isLoading === true ? <Loading /> : null}
             <img onClick={this.viewTheWindow}
                 src="https://firebasestorage.googleapis.com/v0/b/depot-7bb3e.appspot.com/o/logo.png?alt=media&token=3d889013-f357-4d66-adc2-286bdb367ce6"
                 className="cd-logo"
-            />
+                />
 
             <Link to="/" style={{textDecoration: 'none', color:'#fff' }}><h3 className="header-h3">{isLoggedIn ? `Welcome, ${this.props.user.user.user}!` :'MadModels3d'}</h3></Link>
 
