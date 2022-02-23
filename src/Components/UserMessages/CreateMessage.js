dd1b76e18f6356f7efc4225117a587630c5c69dd
Nefari0 <chrisdemontigny@yahoo.com>
2022-02-23 08:20:34 -0700

    adds "to_user" tag to indicate new message

--- src/Components/UserMessages/CreateMessage.js
+++ src/Components/UserMessages/CreateMessage.js
@@ -8,8 +8,8 @@ import './CreateMessage.css'
 
 export default class CreateMessage extends Component {
 
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
 
         this.state = {
             text:"",
@@ -20,9 +20,9 @@ export default class CreateMessage extends Component {
 
     executeSendMessage = async () => {
         const { text } = this.state
-        const { user_id,conversation_id } = this.props
+        const { user_id,conversation_id,to_user } = this.props
 
-        await axios.post('/api/conversation/user/new',{conversation_id,user_id,text})
+        await axios.post('/api/conversation/user/new',{conversation_id,user_id,text,to_user})
         await this.props.sendToSockets(text,conversation_id)
         // this.props.openMessage(conversation_id) 
         this.setState({text:''})
