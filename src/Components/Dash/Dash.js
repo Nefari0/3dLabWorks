b2b811b55435902213477812499af9092a0056c3
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-29 13:54:43 -0600

    phasing out

--- src/Components/Dash/Dash.js
+++ src/Components/Dash/Dash.js
@@ -1,12 +1,12 @@
-import { useState,useEffect } from "react";
-import './Dash.css'
-import MessageBoard from "../UserMessages/MessageBoard";
+// import { useState,useEffect } from "react";
+// import './Dash.css'
+// import MessageBoard from "../UserMessages/MessageBoard";
 
-const Dash = (props) => {
+// const Dash = (props) => {
     
-    return(<div className="my-heard-bar " >
-        <MessageBoard/>
-    </div>)
-}
+//     return(<div className="my-heard-bar " >
+//         <MessageBoard/>
+//     </div>)
+// }
 
-export default Dash
+// export default Dash
