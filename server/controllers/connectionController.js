cb17695229c89bc1ea159e780a35365f76bd3e78
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-31 23:07:42 -0600

    adding, removing, requesting, confirming

--- server/controllers/connectionController.js
+++ server/controllers/connectionController.js
@@ -7,7 +7,7 @@ module.exports = {
         const friends = await db.friends.get_user_friends([user_id])
         return res.status(200).send(friends)
     },
-
+    //  --- get information associated with connection --- //
     getFriendInfo: async (req,res) => {
         const { user_id } = req.params
         const db = req.app.get('db')
@@ -20,6 +20,7 @@ module.exports = {
         const db = req.app.get('db')
         const friends = await db.friends.get_pending_friends([user_id])
         if(friends.length < 1){
+            console.log('not a friend')
             return res.status(404).send('no pending request')
         }
         console.log('from pending',friends)
@@ -29,14 +30,24 @@ module.exports = {
     addFriend: async (req,res) => {
         const { id,friend_id } = req.body
         const db = req.app.get('db')
-        const existingFriend = await db.friends.existing_friend([id,friend_id])
-        if(existingFriend) {
+        // --- has sender already sent a request to this recipient --- //
+        const existingReciever = await db.friends.existing_friend([id,friend_id])
+        if(existingReciever[0] != undefined) {
             return res.status(409).send('already exists')
         }
+        // --- has the recipient send a request to sender --- //
+        const existingSender = await db.friends.existing_friend([friend_id,id])
+        if(existingSender[0] != undefined) {
+            return res.status(409).send('already exists')
+        }
+        
+        // --- send request --- //
         const friend = await db.friends.add_friend([id,friend_id,false])
-        return res.status(200).send(friend)
+        console.log('hit bottom',friend[0])
+        // return res.status(200).send(friend)
     },
     
+    // --- user confirms another user's request --- //
     confirmRequest: async (req,res) => {
         const { from,to,yes } = req.body
         const db = req.app.get('db')
@@ -47,7 +58,7 @@ module.exports = {
 
     removeConnection: async (req,res) => {
         const { from,to } = req.body
-        console.log('hit remove in backend',req.body)
+        // console.log('hit remove in backend',req.body)
         const db = req.app.get('db')
         const xMyFriend = await db.friends.remove_connection([from,to])
         const xYourFriend = await db.friends.remove_connection([to,from])
