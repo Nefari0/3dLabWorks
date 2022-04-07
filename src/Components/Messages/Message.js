8b3e568384058e098f4a10f362be5629a40d647c
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-07 11:26:35 -0600

    rebuilt "contact admin" section

--- src/Components/Messages/Message.js
+++ src/Components/Messages/Message.js
@@ -1,16 +1,67 @@
 import './Messages.css'
+import React from 'react'
+import  { Component } from 'react'
+import photo from './admin_photo1.jpg'
 
-const Message = (props) => {
+class Message extends Component {
+    constructor() {
+        super();
 
-    const { text,send_from } = props
+        this.state = {
+            showActualMessage:true,
+            contactAdmin:false,
+            messageContent:'Enter Message',
+            email:'email',
+        }
+        this.hideGreeting = this.hideGreeting.bind(this)
+    }
 
-    return(
-        <div className='content-box message-box'>
-            <section className='from-name'><p className="name-text">from {send_from}</p></section>
-            <section className='message-content-container'><p className="content-text">{text}</p></section>
-            
-        </div>
-    )
+    hideGreeting() {
+        // console.log('hit hover')
+        this.setState({showActualMessage:false})
+    }
+
+    setContactAdmin() {
+        this.setState({contactAdmin:!this.state.contactAdmin})
+    }
+
+    inputHandler = (prop,val) => {
+        this.setState({[prop]:val})
+    }
+
+    render() {
+
+        const { showActualMessage,contactAdmin,messageContent,email } = this.state
+
+        return(
+            <div>
+            <div className={`messages-container`}>
+                {contactAdmin === false ? <div 
+                onMouseEnter={this.hideGreeting} 
+                className={`actual-message ${showActualMessage ? true : 'hidden'}`}  >
+                    <p className='contact-admin-text' >Welcome to MadModels3d. Click here to contact me</p>
+                    <img src={photo} className='admin-message-photo' />
+                </div> : null}
+
+                {contactAdmin === false ? <svg onClick={() => this.setContactAdmin()} xmlns="http://www.w3.org/2000/svg" class="admin-message-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                </svg> : null}
+
+            </div>
+                {contactAdmin=== true ? <div className='send-message'>
+                    <div className='send-message-title'>
+                        <h2 style={{textTransform:'none'}} >Contact</h2>
+                        <svg onClick={() => this.setContactAdmin()} className="contact-admin-close-button"  xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
+                        <textarea onChange={(e) => this.inputHandler("email",e.target.value)} style={{minHeight:'25px',marginTop:'40px'}} name="text" rows="0" cols="35" wrap="soft" placeholder={email} />
+                        <textarea onChange={(e) => this.inputHandler("messageContent",e.target.value)} style={{minHeight:'45px',marginTop:'10px'}} name="text" rows="5" cols="35" wrap="soft" placeholder={messageContent} />
+                        <div className='contact-admin-send-button' ><p style={{color:'#fff'}} >send</p></div>
+                    </div>
+                </div> : null}
+            </div>
+        )
+    }
 }
 
 export default Message
