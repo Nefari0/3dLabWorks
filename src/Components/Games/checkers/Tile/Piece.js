27decbb4b2372a57b19b7d4ac19923ec5ba10a8a
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-09 05:23:02 -0600

    fixed styling conflict

--- src/Components/Games/checkers/Tile/Piece.js
+++ src/Components/Games/checkers/Tile/Piece.js
@@ -6,9 +6,9 @@ const Piece = (props) => {
     return (
         <div>
             {props.getCurrent('player') ==='good' ? 
-            <div className={`good-player ${props.getCurrent('isInGame') ? false : 'hidden'}`}>{props.getCurrent('isKing') === true ? <p className='player-text'>king</p> : null}</div>
+            <div className={`good-player ${props.getCurrent('isInGame') ? false : 'hide-game-piece'}`}>{props.getCurrent('isKing') === true ? <p className='player-text'>king</p> : null}</div>
         :
-            <div className={`bad-player ${props.getCurrent('isInGame') ? false : 'hidden'}`}>{props.getCurrent('isKing') === true ? <p className='player-text'>king</p> : null}</div>}
+            <div className={`bad-player ${props.getCurrent('isInGame') ? false : 'hide-game-piece'}`}>{props.getCurrent('isKing') === true ? <p className='player-text'>king</p> : null}</div>}
             
         </div>
     )
