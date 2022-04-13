03910f248a647d81b9fb914ae695278dd1f1e788
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-12 23:22:55 -0600

    loads featured videos to Home

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -2,8 +2,14 @@
 module.exports = {
     getVids: async (req,res) => {
         const db = req.app.get('db')
-        console.log('hit video controller')
+        // console.log('hit video controller')
         const videos = await db.videos.get_videos()
         return res.status(200).send(videos)
+    },
+
+    getFeaturedVids: async (req,res) => {
+        const db = req.app.get('db')
+        const videos = await db.videos.featured_videos()
+        res.status(200).send(videos)
     }
 }
