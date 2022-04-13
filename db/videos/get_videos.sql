d69cfc35813bb16a3f4a30df36faa409c379d1e2
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-12 23:27:37 -0600

    fetch videos and associated files

--- db/videos/get_videos.sql
+++ db/videos/get_videos.sql
@@ -2,7 +2,7 @@
 -- FROM d_videos dv
 -- JOIN models m ON m.model_id = dv.model_id
 
-SELECT du.photo_url, du.user_name, m.model_id, m.firebase_url, dv.category, dv.tag, dv.video_url, dv.video_id
+SELECT du.photo_url, du.user_name, m.model_id, m.name, m.firebase_url, dv.category, dv.tag, dv.video_url, dv.video_id
 from d_user du
 join models m on du.user_id = m.user_id
 join d_videos dv on dv.model_id = m.model_id
