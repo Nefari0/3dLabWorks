6ecf77b2b1a703038b512e0ddcbdf1fb40987bcd
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-12 23:26:21 -0600

    users have the option to hide their projects if they want them to be private

--- db/join_user_project.sql
+++ db/join_user_project.sql
@@ -1,5 +1,6 @@
 SELECT * FROM d_user d
 JOIN models m ON d.user_id = m.user_id
+WHERE NOT is_hidden = TRUE
 
 -- SELECT COUNT(*), ml.model_id FROM models m
 -- JOIN model_likes ml ON m.model_id = ml.model_id
