56e7723577d1b857a3d424948145af0681f4f8ed
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-12 23:59:11 -0600

    fixed coding error

--- db/join_featured.sql
+++ db/join_featured.sql
@@ -1,4 +1,4 @@
 SELECT * FROM featured_projects fp
 JOIN models m on fp.model_id = m.model_id
+JOIN d_user d on m.user_id = d.user_id
 WHERE NOT is_hidden  = TRUE
--- JOIN d_user d on m.user_id = d.user_id
