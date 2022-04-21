3da7cea98ce21fc75d8d7d030f6cc398b2e5212e
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-21 14:54:54 -0600

    get memos query

---
+++ db/memo/memo_seed.sql
@@ -0,0 +1,11 @@
+
+-- CREATE TABLE d_memos (
+--   title VARCHAR(250),
+--   body VARCHAR(1000)
+-- )
+
+-- INSERT INTO d_memos (title,body)
+-- VALUES (
+--   'first memo',
+--   'this is the first memo written  for MadModels3d'
+-- )
