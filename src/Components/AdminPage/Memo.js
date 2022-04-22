bd58ea2d1f74cf08cd6cdc65ebe461283ecda4dd
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-22 08:04:56 -0600

    functionality and styling

--- src/Components/AdminPage/Memo.js
+++ src/Components/AdminPage/Memo.js
@@ -1,9 +1,20 @@
 import './Memo.css'
+import { useState } from 'react'
+import axios from 'axios'
 
 const Memo = (props) => {
 
-    return(<div className="text-comtainer" >
-        {/* <textarea></textarea> */}
+    const { body,title,memo_id } = props
+    var [ text,setText ] = useState(body)
+    
+    const sendUpdate = () => {
+        axios.post('/api/memo/update', {text,memo_id})
+    }
+
+    return(<div className="text-container" >
+        <input value={title} style={{width:'95%',marginBottom:'5px'}}></input>
+        <textarea name="text" rows="23" cols="50" wrap="soft" placeholder='text' onChange={(e) => setText(e.target.value)} value={text} className="admin-memo-body" > </textarea>
+        <button onClick={() => sendUpdate()} >submit</button>
     </div>)
 }
 
