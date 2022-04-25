2ee6fb91aa980bc670ce5153821f4137272b7c0b
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-25 16:39:47 -0600

    phasing this components out

--- src/Components/UserPage/AddProject.js
+++ src/Components/UserPage/AddProject.js
@@ -1,48 +1,48 @@
 
-import './UserPage.css'
+// import './UserPage.css'
 
 
-const AddProject = (props) => {
+// const AddProject = (props) => {
 
-    const {previewImageFile} = props
+//     const {previewImageFile} = props
 
-    return(
-        <div className="add-project">
-            <div className="add-project-info-section" >
-                <p>add project</p>
-                <a className="small-text">image</a>
-                <div className="add-project-button">
-                    <input
-                        type="file"
-                        accept="image/png,image/jpeg"
-                        onChange={e => props.handlePhoto(e)} 
-                    />
+//     return(
+//         <div className="add-project">
+//             <div className="add-project-info-section" >
+//                 <p>add project</p>
+//                 <a className="small-text">image</a>
+//                 <div className="add-project-button">
+//                     <input
+//                         type="file"
+//                         accept="image/png,image/jpeg"
+//                         onChange={e => props.handlePhoto(e)} 
+//                     />
                     
-                </div>
-                <a className="small-text">.blend/.stl</a>
-                <div className="add-project-button">
-                    <input
-                        type="file"
-                        accept=".stl,.blend"
-                        onChange={e => props.handleFile(e)} 
-                    />
-                </div>
-                <input placeholder="name" onChange={e => props.handleAddText('projectName', e.target.value)} />
-                {/* <input placeholder="description"/> */}
-                <input placeholder="description" onChange={e => props.handleAddText('projectDescription', e.target.value)} />
+//                 </div>
+//                 <a className="small-text">.blend/.stl</a>
+//                 <div className="add-project-button">
+//                     <input
+//                         type="file"
+//                         accept=".stl,.blend"
+//                         onChange={e => props.handleFile(e)} 
+//                     />
+//                 </div>
+//                 <input placeholder="name" onChange={e => props.handleAddText('projectName', e.target.value)} />
+        
+//                 <input placeholder="description" onChange={e => props.handleAddText('projectDescription', e.target.value)} />
                 
-                <div className="add-project-button">
-                    <button onClick={props.fileHandler}>submit</button>
-                    <button onClick={props.fileHandlerRemove}>clear</button>
-                </div>
-                <div>
-                    <button onClick={props.addNewProject}>cancel</button>
-                </div>
-            </div>
-            <div >
-                <img className="add-project-photo-section" src={previewImageFile} />
-            </div>
-        </div>
-    )
-}
-export default AddProject
+//                 <div className="add-project-button">
+//                     <button onClick={props.fileHandler}>submit</button>
+//                     <button onClick={props.fileHandlerRemove}>clear</button>
+//                 </div>
+//                 <div>
+//                     <button onClick={props.addNewProject}>cancel</button>
+//                 </div>
+//             </div>
+//             <div >
+//                 <img className="add-project-photo-section" src={previewImageFile} />
+//             </div>
+//         </div>
+//     )
+// }
+// export default AddProject
