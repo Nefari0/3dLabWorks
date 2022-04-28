975a6872a6f79eecd4844fb006b8274588bd1ba3
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-27 23:43:20 -0600

    can clear url storage in state

--- src/Components/FeaturedProjects/EditProject/ImagePreview.js
+++ src/Components/FeaturedProjects/EditProject/ImagePreview.js
@@ -2,13 +2,19 @@ import './ImagePreview.css'
 
 const ImagePreview = (props) => {
 
-    const { previewImageFile } = props
+    const { previewImageFile,photo_url }= props
+
+    const clearState = () => {
+        
+        props.handleInput('previewImageFile',null)
+        props.handleInput('photo_url',null)
+    }
 
     return(
         <div className='im-prev-container'>
             <img src={previewImageFile} style={{width:'100%',height:'100%'}} />
-            <div className='edit-file-button' style={{position:'relative',top:'-36px',height:'30px',width:'80px'}} onClick={() => props.passNewPhotoToFB()} ><p>Submit</p></div>
-
+            <div className='edit-file-button' onClick={() => props.addPhoto(photo_url)} ><p>Submit</p></div>
+            <div className='edit-file-button' style={{right:'0px'}} onClick={() => clearState(['string'])} ><p>Cancel</p></div>
         </div>
     )
 }
