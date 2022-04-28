31c83f3db8e13f2c1574b6e70c3ff2e16c3f3dfb
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-27 23:54:01 -0600

    firebase reducer

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -3,13 +3,16 @@ import reduxPromiseMiddleware from 'redux-promise-middleware'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import userReducer from './userReducer'
 import modelsReducer from './modelsReducer'
+import firebaseReducer from './firebaseReducer'
+import thunk from 'redux-thunk'
 
 
 const rootReducer = combineReducers({
   // characters: breakingBadReducer,
   user: userReducer,
   // projects: projectsReducer,
-  models: modelsReducer
+  models: modelsReducer,
+  firebase: firebaseReducer
 
 })
 
