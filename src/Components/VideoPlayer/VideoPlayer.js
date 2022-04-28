a05cc675d67d9990f03507449873458b8fd33435
Nefari0 <chrisdemontigny@yahoo.com>
2022-04-28 07:02:09 -0600

    code fix

--- src/Components/VideoPlayer/VideoPlayer.js
+++ src/Components/VideoPlayer/VideoPlayer.js
@@ -9,7 +9,7 @@ const VideoPlayer = (props) => {
     return(
         <div className='player-container' >
             <header className='video-title' >
-                ><img className='video-user-photo' src={photo_url} />
+                <img className='video-user-photo' src={photo_url} />
                 <div className='vidnm-and-usrnm' > <p className="dark-text video-nm-txt" >{name === 'dummy project' ? video_name : name}</p><p className='usr-nm-txt' style={{marginRight:'60px'}} >Video by {user_name}</p> </div>
             </header>
             <div className='' style={{marginTop:'50px'}} >
