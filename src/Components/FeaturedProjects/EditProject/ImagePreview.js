cafb28ecb816e79b6fc62d325b4f8ea09381bb51
Nefari0 <chrisdemontigny@yahoo.com>
2022-05-01 11:22:55 -0600

    image resizer active

--- src/Components/FeaturedProjects/EditProject/ImagePreview.js
+++ src/Components/FeaturedProjects/EditProject/ImagePreview.js
@@ -12,7 +12,7 @@ const ImagePreview = (props) => {
 
     return(
         <div className='im-prev-container'>
-            <img src={previewImageFile} style={{width:'100%',height:'100%'}} />
+            <img src={previewImageFile} />
             <div className='edit-file-button' onClick={() => props.addPhoto(photo_url)} ><p>Submit</p></div>
             <div className='edit-file-button' style={{right:'0px'}} onClick={() => clearState(['string'])} ><p>Cancel</p></div>
         </div>
