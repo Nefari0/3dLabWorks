5691abe4c70019d07d57323d238d2f09e903037f
Nefari0 <chrisdemontigny@yahoo.com>
2022-05-12 21:38:53 -0600

    new methods

--- server/controllers/photoController.js
+++ server/controllers/photoController.js
@@ -3,6 +3,7 @@ module.exports = {
     getAlbums: async (req,res) => {
         const { user_id } = req.params
         const album = await req.app.get('db').user_photos.get_albums_by_user([user_id])
+        // console.log('hit controller',album)
         return res.status(200).send(album)
     },
 
@@ -13,14 +14,29 @@ module.exports = {
     },
 
     uploadPhoto: async (req,res) => {
-        const { album_id,image_url, } = req.body
-        const photo = await req.app.get('db').user_photos.add_to_album([album_id,image_url])
+        const { album_id,id,image_url, } = req.body
+        const user_id = id
+        const photo = await req.app.get('db').user_photos.add_to_album([album_id,user_id,image_url])
         return res.status(200).send(photo)
     },
 
-    deletePhoto: async (req,res) => {
-        const { photo_id } = req.params
-        const photo = req.app.get('db').user_photos.delete_photo([photo_id])
+    // deletePhoto: async (req,res) => {
+    //     const { photo_id } = req.params
+    //     const photo = req.app.get('db').user_photos.delete_photo([photo_id])
+    //     return res.status(200).send(photo)
+    // },
+
+    getAllPhotos: async (req,res) => {
+        const { user_id } = req.params
+        console.log('hit controller',user_id)
+        const photos = await req.app.get('db').user_photos.get_all_photos([user_id])
+        return res.status(200).send(photos)
+    },
+
+    deletePhotoByUrl: async (req,res) => {
+        const { image_url } = req.body
+        // console.log('hit url ', image_url)
+        const photo = await req.app.get('db').user_photos.delete_by_url([image_url])
         return res.status(200).send(photo)
     }
 }
