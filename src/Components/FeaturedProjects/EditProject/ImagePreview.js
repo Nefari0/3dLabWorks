f8ea187492ce995f9a0604f87e0c1a9c5d5d0445
Nefari0 <chrisdemontigny@yahoo.com>
2022-05-12 21:42:10 -0600

    made adjustments to allow use of this component globally

--- src/Components/FeaturedProjects/EditProject/ImagePreview.js
+++ src/Components/FeaturedProjects/EditProject/ImagePreview.js
@@ -4,8 +4,12 @@ const ImagePreview = (props) => {
 
     const { previewImageFile,photo_url }= props
 
+    const addNewHandler = async () => {
+        await props.addPhoto(photo_url)
+        await clearState()
+    }
+
     const clearState = () => {
-        
         props.handleInput('previewImageFile',null)
         props.handleInput('photo_url',null)
     }
@@ -13,7 +17,7 @@ const ImagePreview = (props) => {
     return(
         <div className='im-prev-container'>
             <img src={previewImageFile} />
-            <div className='edit-file-button' onClick={() => props.addPhoto(photo_url)} ><p>Submit</p></div>
+            <div className='edit-file-button' onClick={() => addNewHandler()} ><p>Submit</p></div>
             <div className='edit-file-button' style={{right:'0px'}} onClick={() => clearState(['string'])} ><p>Cancel</p></div>
         </div>
     )
