bf036ba21a11f6243f0323c23061c5f4fab62bda
Nefari0 <chrisdemontigny@yahoo.com>
2022-05-16 23:34:21 -0600

    users can change background photo

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,11 +15,24 @@ module.exports = {
         const db = req.app.get('db')
         // console.log('this is req.params',req.params)
         // console.log('this is req.body',req.body)
-        const { photo_url } = req.body
-        const { user_id } = req.params
-        console.log('this is from update user',photo_url,user_id)
-        const user = await db.update_user([photo_url,user_id]);
+        const { image_url,user_id } = req.body
+        // const { user_id } = req.params
+        // console.log('this is from update user',photo_url,user_id)
+        const user = await db.update_user([image_url,user_id]);
         return res.status(200).send(user)
-    }
+    },
 
+    updateBackgroundPhoto: async (req,res) => {
+        const { image_url,user_id } = req.body
+        // console.log('hit background controller',image_url)
+        const photo = req.app.get('db').user.update_background_url([image_url,user_id])
+        res.status(200).send(photo)
+    },
+
+    updateInfo: async (req,res) => {
+        const { email,last_name,first_name,user_id } = req.body
+        // console.log('hit update user backend ', first_name)
+        const info = await req.app.get('db').user.update_info([first_name,last_name,email,user_id])
+        return res.status(200).send(info)
+    }
 }
