290e114c8f6d55bce499762dce5c2005a4eefb63
Nefari0 <chrisdemontigny@yahoo.com>
2022-05-23 12:30:25 -0600

    building svg storage to better organize code

---
+++ src/Components/SVG.js
@@ -0,0 +1,35 @@
+
+// const SVG = (params) => {
+//     console.log('hit SVG',params)
+//     // const val = props
+
+//     const downloadIcon =  
+// <div>                            <svg className="small-icon" style={{marginLeft:'10px',marginRight:'5px', height:'50px',width:'50px',opacity:'60%'}} xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+//                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+//                         </svg>
+//     <svg className="small-icon" style={{marginLeft:'10px',marginRight:'5px', height:'50px',width:'50px',opacity:'60%'}} xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+//     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+// </svg>
+// </div>
+
+
+//     switch (params) {
+//         case "download-ion":
+//             return ( <div>
+//                 {downloadIcon}
+//             </div>)
+      
+//     }
+
+//     const param =                         <svg className="small-icon" style={{marginLeft:'10px',marginRight:'5px', height:'50px',width:'50px',opacity:'60%'}} xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+//     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+// </svg>
+
+//     return <div>
+//         {downloadIcon}
+
+//     </div>
+
+// }
+
+// export default SVG
