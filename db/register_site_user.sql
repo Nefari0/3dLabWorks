e25b30aca6b537ed746ad135f8429880932a0e42
Nefari0 <chrisdemontigny@yahoo.com>
2022-05-24 09:00:44 -0600

    adds default placeholder photo upon registering new user

--- db/register_site_user.sql
+++ db/register_site_user.sql
@@ -1,3 +1,3 @@
-INSERT INTO d_user (user_name,hash,email,first_name,is_admin,is_sudo)
-VALUES($1,$2,$3,$4,$5,$6)
+INSERT INTO d_user (user_name,hash,email,first_name,is_admin,is_sudo,photo_url)
+VALUES($1,$2,$3,$4,$5,$6,$7)
 RETURNING *;
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,7 +32,8 @@ module.exports = {
 
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(password,salt);
-        const registeredUser= await db.register_site_user([user_name, hash, email, first_name, is_admin, is_sudo])
+        const photo_url = "https://firebasestorage.googleapis.com/v0/b/depot-7bb3e.appspot.com/o/Chris%20deMontigny%2Fphotos%2Fprofile-pic-placeholder.JPEG?alt=media&token=0f93dc94-dd77-4072-82bd-925a3b19acf3"
+        const registeredUser= await db.register_site_user([user_name, hash, email, first_name, is_admin, is_sudo, photo_url])
         const user = registeredUser[0];
 
   
