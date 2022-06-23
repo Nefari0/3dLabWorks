528ef0703c20937720b12038d9791d29eb173f2c
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-22 23:26:44 -0600

    adjustments

--- src/Components/Document.js
+++ src/Components/Document.js
@@ -8,7 +8,7 @@ const Document = (props) => {
         {display ? <div className="doc-containers">
             {/* <h4 className="about-h4">about</h4> */}
             <h4 className="about-h4">{tag}</h4>
-            <div className="h4-border"></div>
+            <span className="h4-border"></span>
             <p className="long-text">
                 {content}
             </p>
