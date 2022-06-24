ee69ef67ee9a020c1634850410a411b88b38ebbf
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-23 20:44:24 -0600

    major code refactoring. styling

--- src/Components/FeaturedProjects/ProjectPhotos.js
+++ src/Components/FeaturedProjects/ProjectPhotos.js
@@ -32,7 +32,7 @@ const ProjectPhotos = (props) => {
     const [ openMenu,setOpenMenu ] = useState(false)
 
     return(
-        <div className="left">
+        <section>
             {maker_id === id ? <svg xmlns="http://www.w3.org/2000/svg" className="edit-photo-button" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} onClick={() => setOpenMenu(!openMenu)} >
                 {openMenu === false ? <path strokeLinecap="round" strokeLinejoin="round" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />:
                 <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />}
@@ -59,8 +59,10 @@ const ProjectPhotos = (props) => {
                 </div>
 
             </div> : null}
-            <div className={`detail-photo-main`}><img src={url} className="detail-photo"/></div>
-        </div>
+
+            <img src={url} style={{width:'100%',height:'auto'}} />
+
+        </section>
     )
 }
 
