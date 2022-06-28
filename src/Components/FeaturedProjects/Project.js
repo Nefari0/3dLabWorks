8648ebe4528311e438c908fc9492fc8f97f6d528
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-27 19:53:37 -0600

    styling

--- src/Components/FeaturedProjects/Project.js
+++ src/Components/FeaturedProjects/Project.js
@@ -72,57 +72,60 @@ const Project = (props) => {
     }
 
     return(
-        <div className='project-container'>
+        <div className='project-container' style={{overflow:'hidden'}}>
 
-            <header>
+            <header style={{backgroundColor:''}}>
                 <Link to={`/viewuser/${user_id}`} ><img src={getPhotoUrl()} className="project-user-photo"/></Link>
-                <h4 style={{backgroundColor:'',paddingBottom:'5px',paddingTop:'15px'}}>{name}
+                <p className='dark-text' style={{backgroundColor:'',paddingBottom:'15px',paddingTop:'',fontSize:'16px'}}>{name}
                     <br/><i>by {getUserName()}</i>
-                </h4>
+                </p>
             </header>
 
             <Link to={`/projectdetails/${model_id}`}>
-                <div className="image-div" >
-                    <img className="model-photo" src={firebase_url01}/>
-                </div>
+                <section className="project-image">
+                    <img src={firebase_url01}/>
+                </section>
             </Link>
 
-            <section>
+            <section style={{backgroundColor:''}} >
      
 
                 <a onClick={() => isLoggedIn === true ? window.open(firebase_url) : plsSignIn()} >
                     <SVG params={'download_icon'} fill={'none'} stroke={'currentColor'}/>
-                </a> 
+                </a>
                     
-                    { isLoggedIn ? <a className="project-text" href={`${firebase_url}`} target="_blank" rel="noopener noreferrer">download</a> : <a className="project-text" onClick={() => plsSignIn()}>download</a>}
+                { isLoggedIn ? 
+                <a style={{marginRight:'10px'}} href={`${firebase_url}`} target="_blank" rel="noopener noreferrer">download</a>
+                    : 
+                <a style={{marginRight:'10px',color:'blue'}} onClick={() => plsSignIn()}>download</a>}
 
-                    <div className="like-share">
-               
-                            <aside style={{borderBottom:'1px solid #555'}}>
-                                {
-                                heart === false ?
+                <div className="like-share">
+            
+                    <aside style={{borderBottom:'1px solid #555'}}>
+                        {
+                        heart === false ?
+                
                         
-                                
-                                <svg className="small-icon small-icon-tweaks" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" fill='none' viewBox="0 0 24 24" stroke="currentColor" >
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"  />
-                                </svg>
-                                :
-                                <svg className="small-icon small-icon-tweaks" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-                                </svg>
-                                
-                                }
-                                <p style={{marginLeft:'10px'}}>{numOfLikes}</p>
-                            </aside>
+                        <svg className="small-icon small-icon-tweaks" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" fill='none' viewBox="0 0 24 24" stroke="currentColor" >
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"  />
+                        </svg>
+                        :
+                        <svg className="small-icon small-icon-tweaks" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+                        </svg>
+                        
+                        }
+                        <p style={{marginLeft:'10px'}}>{numOfLikes}</p>
+                    </aside>
 
-                            <aside>
-                                <svg className="small-icon small-icon-tweaks" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
-                                </svg>
-                                <p style={{marginLeft:'10px'}}>share</p>
-                            </aside>
+                    <aside>
+                        <svg className="small-icon small-icon-tweaks" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
+                        </svg>
+                        <p style={{marginLeft:'10px'}}>share</p>
+                    </aside>
 
-                    </div>
+                </div>
             
             </section>
         </div>
