0a559c619e5809bf7382fccb68cda89901c9e8b5
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-27 19:30:47 -0600

    styling

--- src/Components/FeaturedProjects/ProjectDetail.js
+++ src/Components/FeaturedProjects/ProjectDetail.js
@@ -18,8 +18,8 @@ import SVG from '../SVG'
 import '../SVG.css'
 
 const selected = {backgroundColor:'#3c598e'}
-const cfff = {color:'#fff'}
-// const c555 = {color:'#555'}
+const cfff = {color:'#fff'} // -- light
+const c555 = {color:'#555'} // -- dark
 const icon ={
     margin: 'auto',
     marginLeft: '10px',
@@ -258,12 +258,12 @@ class ProjectDetail extends Component {
 
                         {mappedPhoto}
 
-                            <ul>
-                                <li style={viewFiles ? selected : null} onClick={() => this.changeView('viewFiles')} >
-                                    <p style={viewFiles ? {color:'#fff'} : null} >Download Files</p>
-                                </li>
+                        <ul>
+                            <li style={viewFiles ? selected : null} onClick={() => this.changeView('viewFiles')} >
+                                <h4 className='dark-text' style={viewFiles ? cfff : null} >Download Files</h4>
+                            </li>
 
-                                <li onClick={this.clickLike}>
+                            <li onClick={this.clickLike}>
                                 {isLoggedIn === true && myLike === true ? 
                                 <svg  style={icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
@@ -273,28 +273,26 @@ class ProjectDetail extends Component {
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                                 </svg>
                                 }
-                                <p className="dark-text">Like</p>
-                                </li>
+                                <p className="">Like</p>
+                            </li>
 
-                                <li style={viewComments ? selected : null} onClick={() => this.changeView('viewComments')} >
-                                    <SVG params={'comments'} />
-                                    <p style={viewComments ? cfff : null}>Comments</p>
-                                </li>
-                       
-                                <li style={viewEditProject || addedToFavorites ? selected : null} onClick={() => this.changeView(authorized() === true ? 'viewEditProject' : 'viewInfo')}>
+                            <li style={viewComments ? selected : null} onClick={() => this.changeView('viewComments')} >
+                                <SVG params={'comments'} />
+                                <p style={viewComments ? cfff : null}>Comments</p>
+                            </li>
+                    
+                            <li style={viewEditProject || addedToFavorites ? selected : null} onClick={() => this.changeView(authorized() === true ? 'viewEditProject' : 'viewInfo')}>
+                                {authorized() === true ? 
+                                <SVG params={'edit_project'} fill={'none'} stroke={'currentColor'}/> : 
+                                <SVG params={'folder'} fill={'none'} stroke={'currentColor'} />}
+                                <p style={viewEditProject ? cfff : null}>{authorized() ? 'Edit Project' : 'Add to Favorites'}</p>
+                            </li>
 
-                                    {authorized() === true ? 
-                                    <SVG params={'edit_project'} fill={'none'} stroke={'currentColor'}/> : 
-                                    <SVG params={'folder'} fill={'none'} stroke={'currentColor'} />}
-
-                                    <p style={viewEditProject ? cfff : null}>{authorized() ? 'Edit Project' : 'Add to Favorites'}</p>
-                                </li>
-
-                                <li style={viewInfo ? selected : null} onClick={() => this.changeView('viewInfo')}>
-                                    <SVG params={'info'} fill={'none'} stroke={'currentColor'}/>
-                                    <p style={viewInfo ? cfff : null} >Info</p>
-                                </li>
-                            </ul>
+                            <li style={viewInfo ? selected : null} onClick={() => this.changeView('viewInfo')}>
+                                <SVG params={'info'} fill={'none'} stroke={'currentColor'}/>
+                                <p style={viewInfo ? cfff : null} >Info</p>
+                            </li>
+                        </ul>
                     </div>
                     
                     <section className="comment-box">
@@ -304,6 +302,7 @@ class ProjectDetail extends Component {
                                 {viewComments ? 'Comments' : null}
                                 {viewFiles ? 'Download File' : null}
                                 {viewInfo ? 'Information' : null}
+                                {viewEditProject ? 'Edit Project' : null}
                             </h3>
                         </header>
 
