c9392a3fa8b9e53c81cdd7185142f6643bc256d0
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-27 19:35:54 -0600

    cleaning

--- src/Components/FeaturedProjects/Description.js
+++ src/Components/FeaturedProjects/Description.js
@@ -4,7 +4,6 @@ const Description = (props) => {
     const { description } = props
 
     return (
-        // <div className="doc-containers" style={{backgroundColor:'blue'}} >
         <div>
             <p className="doc-containers" style={{color:'#555',maxHeight:'200px'}} >{description}</p>
         </div>        
