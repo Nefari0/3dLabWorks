0ae2f9b1106388bae47bf06f457994d02b162b07
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-30 14:56:57 -0600

    cleaning code

--- src/Components/FeaturedProjects/Project.js
+++ src/Components/FeaturedProjects/Project.js
@@ -76,9 +76,9 @@ const Project = (props) => {
 
             <header style={{backgroundColor:''}}>
                 <Link to={`/viewuser/${user_id}`} ><img src={getPhotoUrl()} className="project-user-photo"/></Link>
-                <p className='dark-text' style={{backgroundColor:'',paddingBottom:'15px',paddingTop:'',fontSize:'16px'}}>{name}
+                <h5 className='dark-text' style={{color:'#555',paddingBottom:'15px',paddingTop:'',fontWeight:'400'}}>{name}
                     <br/><i>by {getUserName()}</i>
-                </p>
+                </h5>
             </header>
 
             <Link to={`/projectdetails/${model_id}`}>
@@ -87,17 +87,17 @@ const Project = (props) => {
                 </section>
             </Link>
 
-            <section style={{backgroundColor:''}} >
-     
+            <footer>
 
-                <a onClick={() => isLoggedIn === true ? window.open(firebase_url) : plsSignIn()} >
+                <div onClick={() => isLoggedIn === true ? window.open(firebase_url) : plsSignIn()} >
                     <SVG params={'download_icon'} fill={'none'} stroke={'currentColor'}/>
-                </a>
+                </div>
                     
                 { isLoggedIn ? 
-                <a style={{marginRight:'10px'}} href={`${firebase_url}`} target="_blank" rel="noopener noreferrer">download</a>
+                <a style={{marginRight:'10px',textDecoration:'none'}} href={`${firebase_url}`} target="_blank" rel="noopener noreferrer">download</a>
                     : 
-                <a style={{marginRight:'10px',color:'blue'}} onClick={() => plsSignIn()}>download</a>}
+                <a style={{marginRight:'10px',color:'#555'}} onClick={() => plsSignIn()}>download</a>
+                }
 
                 <div className="like-share">
             
@@ -106,20 +106,20 @@ const Project = (props) => {
                         heart === false ?
                 
                         
-                        <svg className="small-icon small-icon-tweaks" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" fill='none' viewBox="0 0 24 24" stroke="currentColor" >
+                        <svg className="small-icon" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" fill='none' viewBox="0 0 24 24" stroke="currentColor" >
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"  />
                         </svg>
-                        :
-                        <svg className="small-icon small-icon-tweaks" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            :
+                        <svg className="small-icon" onClick={clickedLike} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
                         </svg>
-                        
                         }
+                        
                         <p style={{marginLeft:'10px'}}>{numOfLikes}</p>
                     </aside>
 
                     <aside>
-                        <svg className="small-icon small-icon-tweaks" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg className="small-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
                         </svg>
                         <p style={{marginLeft:'10px'}}>share</p>
@@ -127,7 +127,7 @@ const Project = (props) => {
 
                 </div>
             
-            </section>
+            </footer>
         </div>
     )
 }
