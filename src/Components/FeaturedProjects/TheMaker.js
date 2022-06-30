6cdc6e83860d680c5b0a67e705634f24848f8fd8
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-30 15:00:02 -0600

    cleaning

--- src/Components/FeaturedProjects/TheMaker.js
+++ src/Components/FeaturedProjects/TheMaker.js
@@ -1,13 +1,14 @@
 import './Project.css'
+import './TheMaker.css'
 
 const TheMaker = (props) => {
 
     const { photo_url,user_name,info } = props
     
     return(
-        <div className="detail-box" style={{margin:'auto',marginTop:'5px'}}>
-                <img src={photo_url} className="project-user-photo" style={{marginTop:'20px'}} />
-                <h4 style={{paddingBottom:'5px',paddingTop:'15px'}}>{info[0].name}<br/><i >by {user_name}</i></h4>          
+        <div className="maker" style={{margin:'auto',marginTop:'5px'}}>
+                <img src={photo_url} className="project-user-photo" style={{marginRight:'20px'}} />
+                <h4 style={{paddingBottom:'0px',paddingTop:'15px'}}>{info[0].name}<br style={{paddingBottom:'10px'}}/><i  >by {user_name}</i></h4>          
         </div>
     )
 }
