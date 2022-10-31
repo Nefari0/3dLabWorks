04925ff336b392135d2aa93d5abfe977eee4bd68
Nefari0 <chrisdemontigny@yahoo.com>
2022-10-31 11:30:33 -0600

    test

--- src/Components/WS.js
+++ src/Components/WS.js
@@ -1,3 +1,3 @@
-export const socketString = `ws://165.227.102.189:8000`
+// export const socketString = `ws://165.227.102.189:8000`
 // export const socketString = `ws://127.0.0.1:8000`
-// export const socketString = `ws://madmodels3d.com:8000`
+export const socketString = `ws://madmodels3d.com:8000`
