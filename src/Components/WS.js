41cec1f58cf7fb860e9e48c6c7bf473f055dca78
Nefari0 <chrisdemontigny@yahoo.com>
2022-10-31 13:27:17 -0600

    Disabling websocket

--- src/Components/WS.js
+++ src/Components/WS.js
@@ -1,3 +1,4 @@
-export const socketString = `ws://165.227.102.189:8000`
+// export const socketString = `ws://165.227.102.189:8000`
+export const socketString = 'wss://165.227.102.189:8000'
 // export const socketString = `ws://127.0.0.1:8000`
 // export const socketString = `wss://https://madmodels3d.com:8000`
