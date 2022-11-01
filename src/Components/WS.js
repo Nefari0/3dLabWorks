c59210fd9715efc44a60ab633770ac0672929136
Nefari0 <chrisdemontigny@yahoo.com>
2022-11-01 10:17:31 -0600

    test launch

--- src/Components/WS.js
+++ src/Components/WS.js
@@ -4,4 +4,4 @@
 // export const socketString = `wss://madmodels3d.com:8000`
 
 //  TESTING //
-export const socketString = `ws://165.227.102.189:8003`
+export const socketString = `wss://165.227.102.189:8000`
