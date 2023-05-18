a718ecd35616538e505a29f0ca9ad72855b6be96
Nefari0 <chrisdemontigny@yahoo.com>
2023-05-18 14:35:25 -0600

    Heart icons

--- src/Components/SVG2.jsx
+++ src/Components/SVG2.jsx
@@ -3,6 +3,17 @@
 //     width:'10px'
 // }
 
+const icon ={
+    margin: 'auto',
+    marginLeft: '10px',
+    marginRight: '10px',
+    height: '40px',
+    width: '40px',
+    opacity: '60%',
+    color: '#6495ed',
+    paddingTop: '10px',
+}
+
 export const RightArrow = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -31,6 +42,22 @@ export const GarbageCan = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" className="friend-menu-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} >
             <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+        </svg>
+    )
+}
+
+export const LargeLightHeart = () => {
+    return (
+        <svg style={icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+        </svg>
+    )
+}
+
+export const LargeDarkHeart = () => {
+    return (
+        <svg style={icon} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
         </svg>
     )
 }
