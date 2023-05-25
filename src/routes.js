445fbc8967657c2c09020498fd63c44aa439e807
Nefari0 <chrisdemontigny@yahoo.com>
2023-05-25 14:40:14 -0600

    The component throwing the error is not currently being used

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,10 @@ import ProjectDetail from './Components/ProjectDetails/ProjectDetail'
 import SecurityTest from './Components/UserPage/SecurityTest'
 import Agreement from './Components/Register/Agreement'
 import AdminPage from './Components/AdminPage/AdminPage'
-import Messages from './Components/Messages/Messages'
+
+// This component is throwing an error
+// import Messages from './Components/Messages/Messages'
+
 import ViewUser from './Components/ViewUser/ViewUser'
 import MessageBoard from './Components/UserMessages/MessageBoard'
 
@@ -31,7 +34,10 @@ export default (
         <Route path="/header" component={Header} />
         <Route path="/agreement" component={Agreement} />
         <Route path="/admin" component={AdminPage} />
-        <Route path="/messages/:user_id" component={Messages} />
+
+        {/* THIS COMPONENT IS THROWING AN ERROR */}
+        {/* <Route path="/messages/:user_id" component={Messages} />  */}
+
         <Route path="/usermessages" component={MessageBoard} />
     </Switch>
 )
