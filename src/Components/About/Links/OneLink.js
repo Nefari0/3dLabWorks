fbf909abe48d01254dce248af34c34fac8186758
Nefari0 <chrisdemontigny@yahoo.com>
2023-10-09 15:50:17 -0600

    links

--- src/Components/About/Links/OneLink.js
+++ src/Components/About/Links/OneLink.js
@@ -6,7 +6,7 @@ const OneLink = (props) => {
 
     return(
         <div>
-            <a href={`https://www.${link_to}` } style={{textDecoration:'none'}}><h4 style={{textDecoration:'none',color:'blue'}}>{link_text}</h4></a>
+            <a href={`${link_to}` } style={{textDecoration:'none'}}><h4 style={{textDecoration:'none',color:'blue'}}>{link_text}</h4></a>
         </div>
     )
 }
