53c09ffd6926251fe65cfc479fa436812c63c39e
Nefari0 <chrisdemontigny@yahoo.com>
2023-10-23 14:03:02 -0600

    It might be hard coded, but I thought it'd still be a good idea to have a photo fallback

--- src/Components/Header/Hamburger/hamburger.component.jsx
+++ src/Components/Header/Hamburger/hamburger.component.jsx
@@ -4,7 +4,7 @@ import { HamburgersContainer } from "./hamburger.styles";
 const Hamburgers = ({photo,isLoggedIn,...props}) => {
     return (
         <HamburgersContainer isLoggedIn={isLoggedIn}{...props}>
-            <img src={photo}/>
+            <img src={photo ? photo : 'https://firebasestorage.googleapis.com/v0/b/depot-7bb3e.appspot.com/o/Chris%20deMontigny%2Fphotos%2Fprofile-pic-placeholder.JPEG?alt=media&token=0f93dc94-dd77-4072-82bd-925a3b19acf3'}/>
             <BurgerIcon />
         </HamburgersContainer>
     )
