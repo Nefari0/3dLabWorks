c958d89eeca21beb0a347e6c8c255f147c7d6343
Nefari0 <chrisdemontigny@yahoo.com>
2023-10-24 08:54:31 -0600

    Login/Logout icon

--- src/Components/SVG2.jsx
+++ src/Components/SVG2.jsx
@@ -92,3 +92,19 @@ export const BurgerIcon = () => {
         </svg>
     )
 }
+
+export const LoginIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" className="header-menu-ico" style={{color:'#fff',marginLeft:'20[x'}} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+        </svg>
+    )
+}
+
+export const LogoutIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" className="header-menu-ico" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+        </svg>
+    )
+}
