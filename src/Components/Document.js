0ffff9c4a0770889b40c26304441f7467b9375ef
Nefari0 <chrisdemontigny@yahoo.com>
2026-04-19 19:14:37 -0600

    Repairing document db

--- src/Components/Document.js
+++ src/Components/Document.js
@@ -1,18 +1,18 @@
 
 const Document = (props) => {
 
-    const { content,tag,display } = props.data
+    const { content,tag,display_document } = props.data
 
     return(
     <div>
-        {display ? <div className="doc-containers">
+         {display_document && <div className="doc-containers">
             {/* <h4 className="about-h4">about</h4> */}
             <h4 className="about-h4">{tag}</h4>
             <span className="h4-border"></span>
             <p className="long-text">
                 {content}
             </p>
-        </div> : null}
+        </div>}
     </div>
     )
 }
